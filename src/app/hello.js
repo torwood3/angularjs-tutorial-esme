ba298fe3caa1b8b35aa961fc18de6bc1d92c4b1c
pamasse <pa.masse@outlook.fr>
2017-11-19 19:21:47 +0100

    Example of directive with ng-click and ng-change

--- src/app/hello.js
+++ src/app/hello.js
@@ -1,6 +1,12 @@
+
+// Un composant est composé d'au moins un template, un fichier HTML, et d'un controlleur, une fonction ou une class
+
 export const hello = {
   template: require('./hello.html'),
-  controller() {
-    this.hello = 'Hello World!';
+  controller($log) { // Injection de dépendance
+    const vm = this;
+    vm.handleOnChange = () => {
+      $log.log(vm.name);
+    };
   }
 };
